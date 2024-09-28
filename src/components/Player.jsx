5e54b5b8e369a54e2854b4b1b9670076fca3893e
frndhoon <frndhoon@gmail.com>
2024-09-28 21:20:18 +0900

    feat: display player name when not editing

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,16 @@
+import { useState } from "react";
+
 export default function Player({ name, symbol }) {
+    const [isEditing, setIsEditing] = useState(false);
+
+
     return (
         <li>
             <span className="player">
-                <span className="player-name">{name}</span>
+                {!isEditing && <span className="player-name">{name}</span>}
                 <span className="player-symbol">{symbol}</span>
             </span>
-            <button>Edit</button>
+            <button onClick={() => setIsEditing(true)}>Edit</button>
         </li>
     )
 }
